a00e7522cb548a5eabc141289fe68fce672bb076
matei.nedelschi <matei.nedelschi@nxp.com>
2023-11-18 22:16:52 +0200

    Implemented the "=" operator overload and copy CTR for both the Row and Zone Classes , also implemented at least 2 constructors for all the classes, finally i worked on the displaySeatsFromZone method which displays all the seats from each zone and also i tested all the functionality for creating rows,zones, adding a row to a zone, adding a zone to an eventlocation, displaying each seat in a Row Class, each row in a Zone class and displaying all per zone in the EventLocation class.

--- MyProject/MyProject/Source.cpp
+++ MyProject/MyProject/Source.cpp
@@ -1,16 +1,22 @@
-#define _CRTDBG_MAP_ALLOC
-#include <stdlib.h>
-#include <crtdbg.h>
 #include <iostream>
 #include "classes.cpp"
 
+using namespace std;
+
 int main() {
-	EventLocation ev("Bucuresti",5000,2,2,20);
-	ev.getAvailableSeats();
-	std::cout << "----" << endl;
-	ev.setAvailableSeats(2,7);
-	ev.buyaSeat(2,7);
-	ev.getAvailableSeats();
-	_CrtDumpMemoryLeaks();
+	Row r1(10);
+	Row r2(7);
+	Row r3(5);
+	Zone z1("VIP", 100, r1);
+	z1.addRow(r2);
+	z1.getZoneseats();
+	cout << endl;
+	Zone z2("Category 1", 50, r3);
+	z2.getZoneseats();
+	cout << endl;
+	EventLocation ev1("Bucharest", z1);
+	ev1.addZone(z2);
+	ev1.displaySeatsFromZones();
+
 	return 0;
 }
--- MyProject/MyProject/classes.cpp
+++ MyProject/MyProject/classes.cpp
@@ -1,10 +1,224 @@
-#define _CRTDBG_MAP_ALLOC
-#include <stdlib.h>
-#include <crtdbg.h>
 #include <iostream>
 #include <string>
 
 using namespace std;
+
+class Row {
+    int noSeats = 0;
+    int* seats = nullptr;
+public:
+    //Setters
+    void setNoSeats(int nrseats) {
+        if (nrseats < 0 || nrseats == 0) {
+            throw exception("Invalid number of seats, enter an integer number bigger than 0.");
+        }
+        this->noSeats = nrseats;
+    }
+
+    void setSeats() {
+        if (this->seats != nullptr) {
+            delete[] this->seats;
+            this->seats = nullptr;
+        }
+        seats = new int[this->noSeats];
+        for (int i = 0; i < this->noSeats; i++) {
+            seats[i] = 0;
+        }
+    }
+
+    void setSeatsWithVector(const int* vector,int sizeofvect) {
+        if (this->seats != nullptr) {
+            delete[] this->seats;
+            this->seats = nullptr;
+        }
+        seats = new int[sizeofvect];
+        for (int i = 0; i < sizeofvect; i++) {
+            seats[i] = vector[i];
+        }
+    }
+
+
+    //Getters
+    int getNoSeats() {
+        return this->noSeats;
+    }
+
+    void getSeats() {
+        if (this->noSeats != 0) {
+            for (int i = 0; i < this->noSeats; i++) {
+                cout << seats[i] << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    //Constructors
+    Row(int noSeats) {
+        setNoSeats(noSeats);
+        setSeats();
+    };
+
+    Row(const int* vector,int sizeofvector) {
+        setNoSeats(sizeofvector);
+        setSeatsWithVector(vector,sizeofvector);
+    };
+
+    Row() {
+
+    };
+
+    //Overloading =
+    Row& operator=(const Row& source) {
+        this->noSeats = source.noSeats;
+        if (this->seats == source.seats) {
+            exit;
+        }
+        else {
+            delete[] this->seats;
+            this->seats = new int[source.noSeats];
+            for (int i = 0; i < source.noSeats; i++) {
+                this->seats[i] = source.seats[i];
+            }
+        }
+        return *this;
+    }
+
+    //Copy CTR
+    Row(const Row& r) {
+        if (this->seats != nullptr) {
+            delete[] this->seats;
+            this->seats=nullptr;
+        }
+        this->seats = new int[r.noSeats];
+        for (int i = 0; i < r.noSeats; i++) {
+            seats[i] = r.seats[i];
+        }
+        this->noSeats = r.noSeats;
+    }
+
+    //Destructor
+    ~Row() {
+        if (this->seats != nullptr) {
+            delete[] this->seats;
+            this->seats = nullptr;
+        }
+    };
+};
+
+class Zone {
+    string zoneName = "";
+    int zoneTicketPrice = 0;
+    int nrRows = 0;
+    Row* zoneRows = nullptr;
+
+public:
+    //Setters
+    void setZoneName(const string zoneName) {
+        this->zoneName = zoneName;
+    }
+
+    void setZoneTicketPrice(int price) {
+        this->zoneTicketPrice = price;
+    }
+
+    void addRow(const Row& r) {
+        this->nrRows += 1;
+        Row* copy = new Row[this->nrRows];
+        copy[this->nrRows - 1] = r;
+        if (this->zoneRows != nullptr) {
+            for (int i = 0; i < this->nrRows - 1; i++) {
+                copy[i] = this->zoneRows[i];
+            }
+            delete[] this->zoneRows;
+            this->zoneRows = nullptr;
+        }
+        this->zoneRows = copy;
+    }
+
+    //Getters
+    string getZoneName() {
+        return this->zoneName;
+    }
+
+    int getZoneTicketPrice() {
+        return this->zoneTicketPrice;
+    }
+
+    int getNrRowsForZone() {
+        return this->nrRows;
+    }
+    
+    void getZoneseats() {
+        if (this->zoneRows != nullptr) {
+            for (int i = 0; i < this->nrRows; i++) {
+                if (zoneRows[i].getNoSeats() != 0) {
+                        zoneRows[i].getSeats();
+                }
+                else {
+                    throw exception("No seats available! Please add at least one row");
+                }
+            }
+        }
+    }
+
+    //Constructors
+    Zone(const string ZoneName, int zoneTicketPrice,Row& r){
+        setZoneName(ZoneName);
+        setZoneTicketPrice(zoneTicketPrice);
+        addRow(r);
+    }
+
+    Zone(const string ZoneName, int zoneTicketPrice) {
+        setZoneName(ZoneName);
+        setZoneTicketPrice(zoneTicketPrice);
+    }
+
+    Zone() {
+        
+    }
+
+    //Overloading =
+    Zone& operator=(const Zone& source) {
+        this->zoneName = source.zoneName;
+        this->zoneTicketPrice = source.zoneTicketPrice;
+        this->nrRows = source.nrRows;
+        if (this->zoneRows == source.zoneRows) {
+            exit;
+        }
+        else {
+            delete[] this->zoneRows;
+            this->zoneRows = new Row[source.nrRows];
+            for (int i = 0; i < source.nrRows; i++) {
+                this->zoneRows[i] = source.zoneRows[i];
+            }
+        }
+        return *this;
+    }
+
+    //Copy CTR
+    Zone(const Zone& z) {
+        if (this->zoneRows != nullptr) {
+            delete[] this->zoneRows;
+            this->zoneRows = nullptr;
+        }
+        this->zoneName = z.zoneName;
+        this->zoneTicketPrice = z.zoneTicketPrice;
+        this->zoneRows = new Row[z.nrRows];
+        for (int i = 0; i < z.nrRows; i++) {
+            this->zoneRows[i] = z.zoneRows[i];
+        }
+        this->nrRows = z.nrRows;
+
+    }
+
+    //Destructor
+    ~Zone() {
+        if (this->zoneRows != nullptr) {
+            delete[] this->zoneRows;
+            this->zoneRows = nullptr;
+        }
+    }
+};
 
 class EventLocation {
 private:
@@ -46,7 +260,7 @@ public:
         return this->nrZones;
     }
 
-    string getZoneNames() {
+    string getAvailableZones() {
         if (this->zones != nullptr) {
             for (int i = 0; i < this->nrZones; i++) {
                 cout << this->zones[i].getZoneName() << " ";
@@ -57,11 +271,25 @@ public:
         }
     }
 
+    void displaySeatsFromZones() {
+        if (this->zones != nullptr) {
+            for (int i = 0; i < this->nrZones; i++) {
+                if (this->zones[i].getNrRowsForZone() != 0) {
+                    cout << "Zone: " << i+1 << endl;
+                    this->zones[i].getZoneseats();
+                }
+                else {
+                    throw exception("Zone got zero rows.");
+                }
+            }
+        }
+    }
+
     // Other processing method
     //void printLocationDetails();
 
     //Constructors
-    EventLocation(const string locationName,const Zone& z) {
+    EventLocation(const string locationName,Zone& z) {
         setLocationName(locationName);
         addZone(z);
     }
@@ -78,125 +306,4 @@ public:
             this->zones = nullptr;
         }
     };
-};
-
-class Zone {
-    string zoneName = "";
-    int zoneTicketPrice = 0;
-    int nrRows = 0;
-    Row* zoneSeats = nullptr;
-
-public:
-    //Setters
-    void zoneName(const string zoneName) {
-        this->zoneName = zoneName;
-    }
-
-    void setZoneTicketPrice(int price) {
-        this->zoneTicketPrice = price;
-    }
-
-    void addRow(const Row& r) {
-        this->nrRows += 1;
-        Row* copy = new Row[nrRows];
-        copy[nrRows - 1] = r;
-        if (this->zoneSeats != nullptr) {
-            for (int i = 0; i < this->nrRows - 1; i++) {
-                copy[i] = this->zoneSeats[i];
-            }
-            delete[] this->zoneSeats;
-            this->zoneSeats = nullptr;
-        }
-        this->zoneSeats = copy;
-    }
-
-    //void getZoneseats() {
-    //    if (this->zoneSeats != nullptr) {
-    //        for (int i = 0; i < this->nrRows; i++) {
-    //            for (int i = 0; i < zoneSeats[i].getNoSeats(); i++) {
-    //                 cout << zoneSeats[i] << endl;
-    //            }
-    //        }
-    //    }
-    //    else {
-    //        throw exception("No seats available! Please add at least one row");
-    //    }
-    //}
-
-    //Getters
-    string getZoneName() {
-        return this->zoneName;
-    }
-
-    int getZoneTicketPrice() {
-        return this->zoneTicketPrice;
-    }
-
-    ~Zone() {
-        if (this->zoneSeats != nullptr) {
-            delete[] this->zoneSeats;
-            this->zoneSeats = nullptr;
-        }
-    };
-
-    friend std::ostream& operator <<(std::ostream& os, const Row& c) {
-        return  os << c.getSeats();
-    };
-};
-
-
-class Row {
-    int noSeats = 0; 
-    int* seats = nullptr;
-
-public:
-    
-    void setNoSeats(int nrseats) {
-        if (nrseats < 0 || nrseats == 0) {
-            throw exception("Invalid number of seats, enter an integer number bigger than 0.");
-        }
-        this->noSeats = nrseats;
-    }
-
-
-    //Setters
-    void setSeats() {
-        if(this->seats!=nullptr){
-            delete[] this->seats;
-            this->seats = nullptr;
-        }
-        seats = new int[this->noSeats];
-        for (int i = 0; i < this->noSeats; i++){
-            seats[i] = 0;
-        }
-    }
-
-    //Getters
-    int getNoSeats() {
-        return this->noSeats;
-    }
-
-    void getSeats() {
-        for (int i = 0; i < this->noSeats; i++) {
-            cout << seats[i] << "\t";
-        }
-        cout << endl;
-    }
-
-   Row(int noSeats,const int* vector) {
-        setNoSeats(noSeats);
-        setSeats();
-    }
-
-   Row() {
-
-   };
-
-
-   ~Row() {
-       if (this->seats != nullptr) {
-           delete[] this->seats;
-           this->seats = nullptr;
-       }
-   };
 };
